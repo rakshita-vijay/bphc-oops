025b3bceb076794fc04f04dbf070cd1a0da070c4
Rakshita Vijay <rakshita.vijay1@gmail.com>
2024-09-28 00:24:28 +0530

    ProductDemo working

--- ProductDemo.java
+++ ProductDemo.java
@@ -13,10 +13,27 @@ class ProductDemo {
     for(int index = 0; index < numberOfProducts; index++, ptr += 3) {
       products.add(new Product(Integer.parseInt(st[ptr]),st[ptr+1],Double.parseDouble(st[ptr+2])));
       // products[index].search(pid);
-      products.get(index).display();
+      // products.get(index).display();
       // System.out.println(Integer.parseInt(st[ptr])  + "," + st[ptr+1] + "," + Double.parseDouble(st[ptr+2]));
+      if (products.get(index).price > Integer.parseInt(st[st.length - 1])) {
+        products.get(index).display();
+      }
     }
 
+    System.out.println();
+
     products.sort(Comparator.comparingInt(Product::getProductNo));
+
+    for (int index = 0; index < products.size(); index++) {
+      products.get(index).display();
+    }
+
+    System.out.println();
+
+    products.sort(Comparator.comparingDouble(Product::getPrice));
+
+    for (int index = 0; index < products.size(); index++) {
+      products.get(index).display();
+    }
   }
 }
